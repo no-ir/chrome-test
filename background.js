898d05c5dae6d7bbf865b141632c324600f5784b
Gerson <bobloblaw@loblaw.com>
2015-09-23 17:16:27 -0700

    changes to onupdated

--- background.js
+++ background.js
@@ -13,13 +13,13 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
 	var re = /.*\.youtube.com\/watch\?v=.*/;
 	var foundMatch = re.exec(tab.url);
-	var foundTab = $('.'+tabId);
+	var foundTab = $('#'+tabId);
 
 	if (tab.status === "complete") {
 		if(foundTab.length === 0 && foundMatch != null) {
 			console.log("new tab");
 			if(tab.id) {
-				var html = '<div class="row ' + tabId + '"><div class="title" id="'+tabId +'"><p>' + (tab.title).slice(0, 20) + '...' + '</p></div> \
+				var html = '<div class="row" id="'+ tabId +'"><div class="title"><p>' + (tab.title).slice(0, 20) + '...' + '</p></div> \
 						  <div class="controls"><ul><li><a href="play"><img src="play-circle-2x.png"></a></li> \
 						  <li><a href="pause"><img src="media-pause-2x.png"></a></li> \
 						  <li><a href="next"><img src="media-skip-forward-2x.png"></a></li> \
@@ -31,11 +31,11 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
 		else if(foundTab.length > 0 && foundMatch == null) {
 			//removing
 			console.log('removing tab from html');
-			$('.'+tabId).remove();
+			$('#'+tabId).remove();
 		}
 		else if(foundTab.length > 0 && foundMatch != null) {
 			//update
-			$('#'+tabId).html('<p>' + (tab.title).slice(0, 20) + '...</p>');
+			$('#'+tabId+'> div p').html('<p>' + (tab.title).slice(0, 20) + '...</p>');
 			console.log('updating info: '+tab.title);
 		}
 	}
@@ -51,10 +51,10 @@ chrome.tabs.onActivated.addListener(function(activeInfo){
 
 chrome.tabs.onReplaced.addListener(function(addedTabId, removedTabId){
 	console.log('replaced tab');
-	$('.'+removedTabId).remove();
+	$('#'+removedTabId).remove();
 })
 
 chrome.tabs.onRemoved.addListener(function(tabId, removeInfo){
 	console.log("removing");
-	$('.'+tabId).remove();
+	$('#'+tabId).remove();
 });
