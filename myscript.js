621d83d29f7ee71f97854d86f44c485c5950535c
boqurant <hertz.legend@gmail.com>
2015-09-24 23:42:44 -0700

    added functionality to control video player

--- myscript.js
+++ myscript.js
@@ -1,1 +1,33 @@
-//chrome.runtime.sendMessage("aphex");
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
+	if( message instanceof Object){
+		var vid = document.getElementsByTagName('video')[0];
+		var op = message.action;
+		
+		switch(op){
+			case "play":
+				console.log("play");
+				vid.play();
+				break;
+				
+			case "pause":
+				console.log("pause");
+				if( !vid.paused )
+					vid.pause();
+				break;
+				
+			case "mute":
+				if( !vid.muted )
+					vid.muted = true;
+				else
+					vid.muted = false;
+
+				break;
+				
+			default:
+				console.log("nothing to do");
+		}
+	}
+	else {
+		console.log("unrecognized message received");
+	}	
+});
