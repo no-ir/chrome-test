af04af1f1f77ec2d3a6da79f81d97fd6e807544d
boqurant <hertz.legend@gmail.com>
2015-09-24 23:42:06 -0700

    added message passing from popup to content script

--- popup.js
+++ popup.js
@@ -1,12 +1,32 @@
+chrome.runtime.sendMessage("psend", function(res){
+	console.log(res);
+	if(res){
+		$('#content').append(res.response);
+	}
+	else
+	{
+		console.log(chrome.runtime.lastError);
+	}
+});
 
-	chrome.runtime.sendMessage("psend", function(res){
-		console.log(res);
-		if(res){
-			$('#content').append(res.response);
-		}
-		else
-		{
-			console.log(chrome.runtime.lastError);
-		}
-	});
+function getTabId(t){
+	return $(t).parent().parent().parent().attr("id");
+}
 
+$(document).on('click', '.play', function(){
+	var tabId = getTabId(this);
+	console.log(tabId);
+	chrome.tabs.sendMessage(parseInt(tabId), {action: "play"});
+});
+
+$(document).on('click', '.pause', function(){
+	var tabId = getTabId(this);
+	console.log(tabId);
+	chrome.tabs.sendMessage(parseInt(tabId), {action: "pause"});
+});
+
+$(document).on('click', '.mute', function(){
+	var tabId = getTabId(this);
+	console.log(tabId);
+	chrome.tabs.sendMessage(parseInt(tabId), {action: "mute"});
+});
